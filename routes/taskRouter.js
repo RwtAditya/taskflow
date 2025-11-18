db3fdf69db559a54d8babbfcb31dfeeb0111b873
unknown <117914221+RwtAditya@users.noreply.github.com>
2025-11-18 17:29:27 +0530

    fixed a bug: space in :id

--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -3,8 +3,8 @@ const taskRouter = Router();
 const taskController = require("../controllers/taskController");
 
 taskRouter.get("/", taskController.getTasks);
-taskRouter.post("/", taskController.addTask);
-taskRouter.put("/:id", taskController.updateTask);
-taskRouter.delete("/:id", taskController.deleteTask);
+taskRouter.post("/", taskController.addTasks);
+taskRouter.put("/:id", taskController.updateTasks);
+taskRouter.delete("/:id", taskController.deleteTasks);
 
 module.exports = taskRouter;
