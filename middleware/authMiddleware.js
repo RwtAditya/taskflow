6c04868b22180871e8dbe9d74c39af78a7219585
unknown <117914221+RwtAditya@users.noreply.github.com>
2025-11-18 17:19:35 +0530

    final ver. changed the wrong nome of jwt secret key

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,11 +5,12 @@ dotenv.config();
 
 exports.verifyToken = (req,res,next) => {
     const token = req.headers['authorization']?.split(" ")[1];
+    console.log(token);
     if(!token){
         return res.status(401).json({message: "Unauthorized"});
     }
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
         if(err) {
             return res.status(403).json({message: "Invalid token"});
         }
